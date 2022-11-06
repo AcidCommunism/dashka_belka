43b84e4949e8a72483a976928d18c6b8668b4aea
MaxZamotamzamota@belkacar.ru <mzamota@belkacar.rumzamota@belkacar.ru>
2022-11-06 19:29:55 +0400

    critical fix

--- src/handlers/actions/custom-job.ts
+++ src/handlers/actions/custom-job.ts
@@ -8,6 +8,10 @@ export const customJobActionHandler = async (bot: Telegraf) => {
 };
 
 async function renderCustomJobMenu(bot: Telegraf, ctx: any) {
+    bot.telegram.sendChatAction(ctx.chat.id, 'upload_photo');
+    await bot.telegram.sendPhoto(ctx.chat.id, {
+        source: 'src/media-content/start-job.png',
+    });
     return await bot.telegram.sendMessage(
         // TODO:
         ctx?.chat?.id ?? 'well, I just hope it never happens for now',
